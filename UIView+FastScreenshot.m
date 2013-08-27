dd195e5c950e3ad839ba67802b115cd0ba659c7c
Anton Bukov <k06aaa@gmail.com>
2013-08-27 23:37:00 +0400

    Fixed crash by multiple tapping screenshot button

--- UIView+FastScreenshot.m
+++ UIView+FastScreenshot.m
@@ -17,8 +17,9 @@
     NSInteger width = screenSize.width * [UIScreen mainScreen].scale;
     NSInteger height = screenSize.height * [UIScreen mainScreen].scale;
     NSInteger bytesPerElement = 4;
-    NSInteger bytesPerRow = (bytesPerElement * width);
-    
+    NSInteger bytesPerRow = bytesPerElement * width;
+    NSInteger totalBytes = bytesPerRow * height;
+
     NSDictionary *properties = @{
         (__bridge NSString *)kIOSurfaceIsGlobal:@YES,
         (__bridge NSString *)kIOSurfaceBytesPerElement:@(bytesPerElement),
@@ -32,23 +33,19 @@
     IOSurfaceRef surface = IOSurfaceCreate((__bridge CFDictionaryRef)properties);
     
     IOSurfaceLock(surface, 0, nil);
-    // Take currently displayed image from the LCD
     CARenderServerRenderDisplay(0, CFSTR("LCD"), surface, 0, 0);
-    // Unlock the surface
+    void * baseAddr = IOSurfaceGetBaseAddress(surface);
+    NSData * data = [NSData dataWithBytes:baseAddr length:totalBytes];
     IOSurfaceUnlock(surface, 0, 0);
     
-    // Make a raw memory copy of the surface
-    void * baseAddr = IOSurfaceGetBaseAddress(surface);
-    NSInteger totalBytes = bytesPerRow * height;
-    
-    CGDataProviderRef provider = CGDataProviderCreateWithData(NULL, baseAddr, totalBytes, NULL);
-    CGImageRef imageRef = CGImageCreate(width, height, 8, 8 * bytesPerElement, 4 * width,
+    CGDataProviderRef provider = CGDataProviderCreateWithData(NULL, data.bytes, data.length, NULL);
+    CGImageRef imageRef = CGImageCreate(width, height, 8, 8*bytesPerElement, bytesPerRow,
                                         CGColorSpaceCreateDeviceRGB(),
                                         kCGBitmapByteOrder32Host | kCGImageAlphaFirst,
                                         provider, NULL, false, kCGRenderingIntentDefault);
     
     UIImage * image = [UIImage imageWithCGImage:imageRef];
-    
+
     CGImageRelease(imageRef);
     CFRelease(surface);
     
